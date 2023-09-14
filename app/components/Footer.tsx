b2dd353357cd7475c85b8f4e4f2f20cffd8c9838
juleni <69912179+juleni@users.noreply.github.com>
2023-09-14 10:43:05 +0200

    Added Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="text-xs sm:text-sm text-center border-t border-slate-300 py-6 mt-6 text-slate-400">
+        2023 Created by JULENI
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
